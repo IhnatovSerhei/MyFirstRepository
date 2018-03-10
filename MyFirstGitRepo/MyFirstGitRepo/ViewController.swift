38e0457a1f9e9f870b3a3e42dffda11c96d3df6e
Serj <ihnatov17@gmail.com>
2018-03-10 13:35:12 +0200

    added project

---
+++ MyFirstGitRepo/MyFirstGitRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstGitRepo
+//
+//  Created by Сергей on 10.03.2018.
+//  Copyright © 2018 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
